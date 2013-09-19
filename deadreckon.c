637d08c2b29149c8acb35f75b7e471839f384ea9
Tobi Hahn <cmpgk1024@gmail.com>
2013-09-19 17:44:04 -0400

    dead reckoning based on rpm and wheel circumferece to go a certain distance, should be moved to a function once tested

---
+++ deadreckon.c
@@ -0,0 +1,20 @@
+foat rpm = 10;
+float circumference = 1.5;
+int distancetogo = 100;
+
+void setMotors(int power){
+	motor[motorB] = power;
+	motor[motorC] = power;
+}
+
+task main(){
+	float timetomove = distancetogo / (rpm * circumference);
+	timetomove *= 60;
+	float timemoved = 0;
+	setMotors(100);
+	while(timemoved < timetomove){
+		Wait1Msec(1)
+		timemoved += 0.001
+	}
+	setMotors(0);
+}
