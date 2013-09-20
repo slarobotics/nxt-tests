c16145896b5a1dfb7087237d09f10c59968b0deb
Tobi Hahn <cmpgk1024@gmail.com>
2013-09-20 11:32:51 -0400

    pause when touch sensor hit

--- deadreckon.c
+++ deadreckon.c
@@ -1,3 +1,4 @@
+const tSensors touchSensor          = (tSensors) S1;   //touch sensor config
 //all units in inches
 float rpm = 30;
 float circumference = 12;
@@ -11,10 +12,15 @@ void moveDistance(float distancetogo){
 	float timetomove = distancetogo / (rpm * circumference);
 	timetomove *= 60;
 	float timemoved = 0;
-	setMotors(100);
 	while(timemoved < timetomove){
 		wait1Msec(1);
-		timemoved += 0.001;
+		if(!SensorValue(touchSensor) == 1){
+			setMotors(100);
+			timemoved += 0.001;
+		}
+		else{
+			setMotors(0);
+		}
 	}
 	setMotors(0);
 }
